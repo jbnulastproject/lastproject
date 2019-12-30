a7b7c031c8814d6d3ccd77b71842bd47a42accf7
chldntjr129 <1st_fly@likelion.org>
2019-12-30 17:36:20 +0900

    lastproject

--- mystorage/admin.py
+++ mystorage/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Essay
+from .models import Album
+from .models import Files
+
+admin.site.register(Essay)
+admin.site.register(Album)
+admin.site.register(Files)
